939d3588a40ca897a49723e9684c0b670cda3ebe
danielleond99 <87273355+danielleond99@users.noreply.github.com>
2024-09-26 14:16:52 -0400

    Initial Commit

---
+++ SwiftUIMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIMap
+//
+//  Created by Daniel Leon on 26/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIMap/SwiftUIMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapApp.swift
+//  SwiftUIMap
+//
+//  Created by Daniel Leon on 26/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
