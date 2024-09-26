47898ea045c6a7d68b7e749989e37253c3fafabf
danielleond99 <87273355+danielleond99@users.noreply.github.com>
2024-09-26 15:03:24 -0400

    add asssets and location data services and model

---
+++ SwiftUIMap/Models/LocationModel.swift
@@ -0,0 +1,19 @@
+//
+//  LocationModel.swift
+//  SwiftUIMap
+//
+//  Created by Daniel Leon on 26/9/24.
+//
+
+import Foundation
+import MapKit
+
+struct LocationModel {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
+        
